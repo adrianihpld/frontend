c7914be282367d6ef78b1a3299f1d2772c4eff77
adrianihpld <139892026+adrianihpld@users.noreply.github.com>
2024-02-14 13:46:43 -0600

    Tested ternary operator

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import CardList from './Components/CardList/CardList';
-import Search from './Components/Search/Search';
-import Search2 from './Components/Search/Search2';
+import { ChangeEvent, SyntheticEvent, useState } from "react";
+import "./App.css";
+import CardList from "./Components/CardList/CardList";
+import Search from "./Components/Search/Search";
+import { searchCompanies } from "./api";
+import { CompanySearch } from "./company";
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>();
+  const [serverError, setServerError] = useState<string | null>(null);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const onClick = async (e: SyntheticEvent) => {
+    const result = await searchCompanies(search);
+    //setServerError(result.data);
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+  };
   return (
     <div className="App">
-      <Search />
-      <Search2 />
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
       <CardList />
+      {serverError && <div>Unable to connect to API</div>}
+      {/* {serverError ? <div>Connected</div> : <div>Unable to connect to api</div>} */}
     </div>
   );
 }
